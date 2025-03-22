5fcf74fb7a7111bade3306ba5c5a138cb2cac88d
SixtenE <sixten.ekblad@gmail.com>
2025-03-22 07:19:35 +0100

    redesign ui + more bots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ChatInterface from '@/components/ChatInterface'
 
 export default function Home() {
   return (
-    <div className='container flex flex-col mx-auto justify-center'>
+    <div className='container flex flex-col mx-auto w-lg '>
       <ChatInterface />
     </div>
   )
--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { FormEvent, useState } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 import { Button } from './ui/button'
-import { Input } from './ui/input'
+
 import {
   Select,
   SelectContent,
@@ -14,22 +14,23 @@ import {
 } from './ui/select'
 import Markdown from 'react-markdown'
 import { Loader2 } from 'lucide-react'
+import { Textarea } from './ui/textarea'
 
 export default function ChatInterface() {
   const [markdown, setMarkdown] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
+  const formRef = useRef<HTMLFormElement>(null)
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault()
     e.stopPropagation()
 
     setLoading(true)
+    setMarkdown(null)
 
     const formData = new FormData(e.target as HTMLFormElement)
     const prompt = formData.get('prompt') as string
     const bot = formData.get('bot') as string
-
-    console.log(prompt, bot)
 
     const response = await fetch('/api/generate', {
       method: 'POST',
@@ -38,37 +39,84 @@ export default function ChatInterface() {
       },
       body: JSON.stringify({ prompt, bot }),
     })
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch response')
+    if (!response.ok || !response.body) {
+      throw response.statusText
     }
 
-    const data = await response.json()
-    setMarkdown(data.message)
+    const { message } = await response.json()
+    setMarkdown(message)
+
     setLoading(false)
   }
 
   return (
-    <div className='flex-col'>
-      <form
-        onSubmit={handleSubmit}
-        className='w-full flex flex-col gap-y-2 px-56 mt-56'
-      >
-        <BotSelect />
-
-        <Input type='text' name='prompt' placeholder='Prompt...' required />
-        <Button type='submit' disabled={loading}>
-          {loading ? <Loader2 className='animate-spin' /> : 'Generate'}
-        </Button>
-      </form>
-      <div className='w-full px-32 mt-12'>
-        {markdown && <Markdown>{markdown}</Markdown>}
-      </div>
+    <div className='flex-col w-full'>
+      {markdown ? (
+        <div className='w-full flex-col flex justify-center mt-12'>
+          <Markdown>{markdown}</Markdown>
+          <Button onClick={() => setMarkdown(null)} className='w-fit mt-12'>
+            New prompt
+          </Button>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className='w-full flex flex-col gap-y-2 mt-24'
+          ref={formRef}
+        >
+          <Textarea
+            name='prompt'
+            placeholder='Prompt...'
+            className='h-32'
+            required
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+                formRef.current?.requestSubmit()
+              }
+            }}
+          />
+          <div className='grid grid-cols-2 gap-x-2'>
+            <BotSelect />
+            <Button type='submit' disabled={loading}>
+              {loading ? <Loader2 className='animate-spin' /> : 'Generate'}
+            </Button>
+          </div>
+        </form>
+      )}
     </div>
   )
 }
 
 function BotSelect() {
+  const bots = [
+    'Kanye West',
+    'Donald Trump',
+    'Shakespeare',
+    'Yoda',
+    'Joker',
+    'Gollum',
+    'Groot',
+    'Gandalf',
+    'Hannibal Lecter',
+    'Homer Simpson',
+    'James Bond',
+    'Kermit the Frog',
+    'John Lennon',
+    'Marge Simpson',
+    'Mickey Mouse',
+    'Morgan Freeman',
+    'Mr. T',
+    'Napoleon Bonaparte',
+    'Nelson Mandela',
+    'Snoop Dogg',
+    'Spongebob Squarepants',
+    'Steve Jobs',
+    'Superman',
+    'The Terminator',
+    'Walter White',
+  ]
+
   return (
     <Select name='bot' required>
       <SelectTrigger className='w-full'>
@@ -77,10 +125,11 @@ function BotSelect() {
       <SelectContent>
         <SelectGroup>
           <SelectLabel>Choose a bot</SelectLabel>
-          <SelectItem value='kanye'>Kanye West</SelectItem>
-          <SelectItem value='trump'>Donald Trump</SelectItem>
-          <SelectItem value='shakespeare'>Shakespeare</SelectItem>
-          <SelectItem value='yoda'>Yoda</SelectItem>
+          {bots.map((bot) => (
+            <SelectItem key={bot} value={bot.toLowerCase()}>
+              {bot}
+            </SelectItem>
+          ))}
         </SelectGroup>
       </SelectContent>
     </Select>
